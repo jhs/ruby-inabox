3355224b3806ec78171cc6be814e794a52b312d5
Jason Smith (air) <jason.h.smith@gmail.com>
2013-11-08 16:32:34 -0800

    Check for the newer Rake now

--- env.sh
+++ env.sh
@@ -192,7 +192,7 @@ main () {
     #    $(which gem) install "$rake_gem"
     #fi
 
-    confirm_build rake "$build/bin/rake" 0.9.2.2 || return 1
+    confirm_build rake "$build/bin/rake" 0.9.6 || return 1
 }
 
 # Hook into a possible parent project's Rake system.
